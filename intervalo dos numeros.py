f73a1cbc0ab773b755d756cc2bf8e5cc758a50b4
l0rennareis <lorennareis1998@gmail.com>
2017-06-23 19:50:13 -0300

    Add files via upload

---
+++ intervalo dos numeros.py
@@ -0,0 +1,7 @@
+#Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.#
+
+n1=int(input("Digite n1: "))
+n2=int(input("Digite n2: "))
+i=1
+i=list(range(n1,n2))
+print(i)
